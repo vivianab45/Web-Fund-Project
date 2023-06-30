3cc380133b34902040161df51a200a46483f76af
vivianab45 <balleza45@gmail.com>
2023-06-30 15:40:29 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var profileImg=document.querySelector ("#pic");
+
+function switchImg2 () {
+    profileImg.src="./images/IMG_2134.jpg";
+    profileImg.alt="pic2";
+}
+
+function switchImg1() {
+    profileImg.src="./images/IMG_2415.JPG";
+    profileImg.alt="personal-pic";
+}
